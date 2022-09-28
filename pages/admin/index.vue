4b61741c18fbd1a1e76733dc359c906cb9edd982
Davron Usmonov <akunin94@gmail.com>
2022-09-28 22:55:22 +0500

    Add analytics page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,11 +1,31 @@
 <template>
-  <h1>Welcome to admin page</h1>
+  <div>
+    <h1>Аналитика по постам</h1>
+    <AppAnalyticsChart 
+      title="Количество просмотров"
+      :labels="views.labels"
+      :data="views.data"
+    />
+    <AppAnalyticsChart 
+      title="Количество комментариев"
+      :labels="comments.labels"
+      :data="comments.data"
+    />
+  </div>
 </template>
 
 <script>
+import AppAnalyticsChart from '../../components/admin/AnalyticsChart';
 export default {
   layout: 'admin',
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'],
+  components: {
+    AppAnalyticsChart
+  },
+  async asyncData({store}) {
+    const {views, comments} = await store.dispatch('post/getAnalytics');
+    return {views, comments};
+  }
 }
 </script>
 
